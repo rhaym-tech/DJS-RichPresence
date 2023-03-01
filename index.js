5d86397a214603a055035c2ce774b2a48e4d5fce
sec1-info <abdoza55555@gmail.com>
2023-03-01 14:03:08 +0100

    initial release

---
+++ index.js
@@ -0,0 +1,42 @@
+const RPC = require('discord-rpc');
+const dotenv = require("dotenv");
+
+dotenv.config()
+
+const client = new RPC.Client({
+    transport: 'ipc'
+});
+
+client.on('ready', async () => {
+    console.log("Loading Rich Presence...");
+    try {
+        await client.request('SET_ACTIVITY', {
+            pid: process.pid,
+            activity: {
+                details: process.env.DETAILS,
+                state: process.env.STATE,
+                timestamps: {
+                    start: Date.now()
+                },
+                assets: { //comment this part if you don't want to add images or comment a specific part
+                    large_image: process.env.LARGE_IMAGE,
+                    large_text: process.env.LARGE_TEXT,
+                    small_image: process.env.SMALL_IMAGE,
+                    small_text: process.env.SMALL_TEXT
+                },
+                buttons: [
+                    { label: process.env.BUTTON_1_LABEL, url: process.env.BUTTON_1_URL },
+                    { label: process.env.BUTTON_2_LABEL, url: process.env.BUTTON_2_URL }
+                ]
+            }
+        });
+        console.log("Rich Presence loaded successfully");
+    } catch (error) {
+        console.error("Error loading Rich Presence", error);
+    }
+});
+
+client.login({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
